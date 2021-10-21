dd970fa81be8686ed7329cf019109b6376e31bd0
ingCarlosPer <ing.carlosper@hotmail.com>
2021-10-20 21:37:34 -0500

    agregando clases para ejercicio de poo en java

---
+++ semillero-servicios/src/main/java/com/semillero/hbt/interfaces/AccionesVehiculoInterface.java
@@ -0,0 +1,16 @@
+package com.semillero.hbt.interfaces;
+
+import com.hbt.semillero.enums.TipoVehiculoEnum;
+
+public interface AccionesVehiculoInterface {
+	
+	public int obtenerVelocidadMaxima();
+	
+	public Long obternerPesoMaximoCarga();
+	
+	public boolean determinarTipoVehiculo(TipoVehiculoEnum tipoVehiculoEmun) throws Exception;
+	
+	public default void acelerar() {
+		System.out.println("El vehiculo ha iniciado acelerar");
+	}
+}
